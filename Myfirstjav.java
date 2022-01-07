1bdf613b39b7305539260c702b9dace08564dbd4
jaadhika <jagadesh.adhikari@gmail.com>
2022-01-07 00:13:03 -0800

    my demo files

---
+++ Myfirstjav.java
@@ -0,0 +1,10 @@
+public class Myfirstjav {
+
+		public static void main(String[] args) {
+					System.out.println("This is my myfirst jav start execution::");
+							for(int i=0; i<=10; i++) {
+											System.out.println("This is my my first jav::" + i);
+													}
+
+								}
+}
